34bee28d22df4725ea78d42f9474554e48dbc80a
Gabriel Raguindin <143593330+GabrielRaguindin@users.noreply.github.com>
2024-06-04 20:25:23 +0800

    Add files via upload

---
+++ Landing Page/Front Page/event.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl, {
+            html: true,
+            content: getPopoverContent(popoverTriggerEl)
+        });
+    });
+
+    function getPopoverContent(popoverTriggerEl) {
+        // Get the data-content attribute value
+        var content = popoverTriggerEl.getAttribute('data-bs-content');
+
+        // Create a custom HTML structure for the popover content
+        return '<ul><li>' + content + '</li></ul>';
+    }
+});
